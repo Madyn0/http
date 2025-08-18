def9a0efb975db3a9ba7675bde41a2fc8f2d7443
Dylan Masson <dylanmmasson@gmail.com>
2025-08-18 22:35:48 +0200

    Basic socket

--- http.c
+++ http.c
@@ -1,7 +1,42 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define PORT 9191
+
+void cleanup(int sock);
 
 int main()
 {
-    printf("foobar\n");
+    struct sockaddr_in host;
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    puts("socket created");
+
+    host.sin_family      = AF_INET;
+    host.sin_port        = htons(PORT);
+    host.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    if (bind(sock, (struct sockaddr *)&host, sizeof(host)) == -1) {
+        perror("bind");
+        cleanup(sock);
+        exit(EXIT_FAILURE);
+    }
+    puts("bind created");
+
+    cleanup(sock);
     return 0;
+}
+
+void cleanup(int sock)
+{
+    if (close(sock) == -1)
+        perror("close");
+
+    puts("cleaned up");
 }
