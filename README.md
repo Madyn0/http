f173f2cd9106935da50060ad7e3fbb1fef583d8b
Dylan Masson <dylanmmasson@gmail.com>
2025-08-18 23:47:54 +0200

    Listen for connections and respond with hard-coded message

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # HTTP Server
+
+
+## How to check if the server is running:
+
+**Using [`ss`](https://man.archlinux.org/man/ss.8):**
+```
+ss -plnt sport = :9191
+```
+
+---
+
+## How to connect to the server:
+
+**Using [`nc`](https://man.archlinux.org/man/nc.1):**
+```bash
+nc localhost 9191
+```
+
+---
--- http.c
+++ http.c
@@ -1,42 +1,61 @@
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define PORT 9191
-
-void cleanup(int sock);
+#define PORT    9191
+#define BACKLOG 1
 
 int main()
 {
-    struct sockaddr_in host;
+    int listen_fd, client_fd;
+    struct sockaddr_in listen_addr, client_addr;
 
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
+    listen_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
-    puts("socket created");
 
-    host.sin_family      = AF_INET;
-    host.sin_port        = htons(PORT);
-    host.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    listen_addr.sin_family      = AF_INET;
+    listen_addr.sin_port        = htons(PORT);
+    listen_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    if (bind(sock, (struct sockaddr *)&host, sizeof(host)) == -1) {
+    if (bind(listen_fd, (struct sockaddr *)&listen_addr,
+             sizeof(listen_addr)) == -1) {
         perror("bind");
-        cleanup(sock);
+        close(listen_fd);
         exit(EXIT_FAILURE);
     }
-    puts("bind created");
 
-    cleanup(sock);
-    return 0;
-}
+    if (listen(listen_fd, BACKLOG) == -1) {
+        perror("listen");
+        close(listen_fd);
+        exit(EXIT_FAILURE);
+    }
 
-void cleanup(int sock)
-{
-    if (close(sock) == -1)
-        perror("close");
+    printf("listening on: %s:%d\n", inet_ntoa(listen_addr.sin_addr),
+           ntohs(listen_addr.sin_port));
 
-    puts("cleaned up");
+    socklen_t client_addr_len = sizeof(client_addr);
+    client_fd =
+        accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_fd == -1) {
+        perror("accept");
+        close(listen_fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr),
+           ntohs(client_addr.sin_port));
+
+    write(client_fd, "foobar\n", 7);
+
+    close(client_fd);
+    puts("client disconnected");
+
+    close(listen_fd);
+    puts("server stopped");
+
+    return EXIT_SUCCESS;
 }
